34dc46c7c306c9c2da0ea1261ff8c361755b96e2
sco-bo <scottebobbitt@gmail.com>
2016-03-15 16:45:14 -0400

    tweak authentication

--- config/initializers/01_flickr_key_load.rb
+++ config/initializers/01_flickr_key_load.rb
@@ -3,5 +3,17 @@ require 'flickraw'
 FlickRaw.api_key = ENV['API_KEY']
 FlickRaw.shared_secret = ENV['SHARED_SECRET']
 
-flickr.access_token = ENV['ACCESS_TOKEN']
-flickr.access_secret = ENV['ACCESS_SECRET']
+token = flickr.get_request_token
+auth_url = flickr.get_authorize_url(token['oauth_token'], :perms => 'delete')
+
+puts "Open this url in your process to complete the authication process : #{auth_url}"
+puts "Copy here the number given when you complete the process."
+verify = gets.strip
+
+begin
+  flickr.get_access_token(token['oauth_token'], token['oauth_token_secret'], verify)
+  login = flickr.test.login
+  puts "You are now authenticated as #{login.username} with token #{flickr.access_token} and secret #{flickr.access_secret}"
+rescue FlickRaw::FailedResponse => e
+  puts "Authentication failed : #{e.msg}"
+end
