61c947fe0a349db6f430392c168d8206155e635b
sco-bo <scottebobbitt@gmail.com>
2016-03-15 16:38:27 -0400

    update initializer

--- config/initializers/01_flickr_key_load.rb
+++ config/initializers/01_flickr_key_load.rb
@@ -2,3 +2,6 @@ require 'flickraw'
 
 FlickRaw.api_key = ENV['API_KEY']
 FlickRaw.shared_secret = ENV['SHARED_SECRET']
+
+flickr.access_token = ENV['ACCESS_TOKEN']
+flickr.access_secret = ENV['ACCESS_SECRET']
