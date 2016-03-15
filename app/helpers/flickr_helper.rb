7b8a54a47611e2cce90fd719772622667b98eb41
sco-bo <scottebobbitt@gmail.com>
2016-03-15 14:10:40 -0400

    styles photo layout in gallery format

--- app/helpers/flickr_helper.rb
+++ app/helpers/flickr_helper.rb
@@ -1,15 +1,10 @@
 
 module FlickrHelper
-  def user_photos(user_id, photo_count = 12)
-    FlickRaw.api_key = ENV['API_KEY']
-    FlickRaw.shared_secret = ENV['SHARED_SECRET']
-    flickr.access_token = ENV['ACCESS_TOKEN']
-    flickr.access_secret = ENV['ACCESS_SECRET']
-    login = flickr.test.login
-    flickr.photos.search(user_id: user_id)
+  def user_photos(user_id, photo_count)
+    flickr.photos.search(user_id: user_id, per_page: photo_count)
   end
 
-  def render_flickr_sidebar_widget(user_id, photo_count = 12, columns = 2)
+  def render_flickr_sidebar_widget(user_id, photo_count = 100)
     begin 
       photos = user_photos(user_id, photo_count)
       render :partial => '/flickr/sidebar_widget', :locals => {:photos => photos}
